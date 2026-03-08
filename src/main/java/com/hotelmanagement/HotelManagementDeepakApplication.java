8c385d8e3e8eb3d60cebe0f038374b2f5f8d4613
Deepak-Kr19 <deepakkr1753@gmail.com>
2026-03-09 02:03:13 +0530

    Hotel Management System Spring Boot Project

---
+++ src/main/java/com/hotelmanagement/HotelManagementDeepakApplication.java
@@ -0,0 +1,14 @@
+package com.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementDeepakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementDeepakApplication.class, args);
+		System.out.println("Hotel-Managemnet Server Started...");
+	}
+
+}
